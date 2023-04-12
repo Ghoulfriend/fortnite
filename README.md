40c49ffad3440dbfc7f098ca1329b7690afe748e
Ghoulfriend <106831003+Ghoulfriend@users.noreply.github.com>
2023-04-12 10:48:48 -0700

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+  <h1>XMA Shit</h1>
+
+
+</div>
+
+
+## Installation
+To use this script, you need a userscript manager.
+
+- Install [Tampermonkey](https://www.tampermonkey.net/) or [Violentmonkey](https://violentmonkey.github.io/get-it/)
+- Then, click on [this link](https://github.com/Ghoulfriend/fortnite/raw/main/xmahidden.user.js) and a new tab should open, prompting you with an installation screen.
+- Hit install and you should be good to go!
---
+++ xmahidden.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         XMA Hidden Hidden
+// @namespace    XMA Hidden Hidden
+// @version      0.269
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.xivmodarchive.com/*
+// @icon         https://www.xivmodarchive.com/favicon.png
+// @grant        none
+// @run-at       document-start
+// @updateURL     https://github.com/Ghoulfriend/fortnite/raw/main/xmahidden.user.js
+// @downloadURL   https://github.com/Ghoulfriend/fortnite/raw/main/xmahidden.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+function contains(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  return Array.prototype.filter.call(elements, function(element){
+    return RegExp(text).test(element.textContent);
+  });
+}
+
+    function doit(){
+contains('a', 'Hidden Mod')[0].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[1].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[2].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[3].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[4].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[5].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[6].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[7].parentElement.parentElement.parentElement.style.display = "none"
+contains('a', 'Hidden Mod')[8].parentElement.parentElement.parentElement.style.display = "none"
+    }
+
+       setTimeout(doit, 100)
+})();
